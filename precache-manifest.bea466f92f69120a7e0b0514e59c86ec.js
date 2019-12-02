998aae60deb6d9f79dbed3330fd0fd48ae33dcae
wasafune <wasafune@gmail.com>
2019-12-02 00:25:46 -0800

    Updates

--- precache-manifest.bea466f92f69120a7e0b0514e59c86ec.js
+++ precache-manifest.bea466f92f69120a7e0b0514e59c86ec.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b897b04141386494c4529236c8875fd6",
+    "revision": "bf2d0fd39fd0e70744fdce24952197dc",
     "url": "/foodtour/index.html"
   },
   {
-    "revision": "5fb78ac1acdbfe0aacb4",
-    "url": "/foodtour/static/css/main.45b47251.chunk.css"
+    "revision": "20a328d076f217b5a25f",
+    "url": "/foodtour/static/css/main.d83f6bd9.chunk.css"
   },
   {
     "revision": "194a8bcb435412af3488",
     "url": "/foodtour/static/js/2.99e9e134.chunk.js"
   },
   {
-    "revision": "5fb78ac1acdbfe0aacb4",
-    "url": "/foodtour/static/js/main.b94b592e.chunk.js"
+    "revision": "20a328d076f217b5a25f",
+    "url": "/foodtour/static/js/main.e05fb8de.chunk.js"
   },
   {
     "revision": "70b913a0512aa1e48fd1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foodtour/precache-manifest.8d58bcb35ec707538480f5f43c9b0485.js"
+  "/foodtour/precache-manifest.bea466f92f69120a7e0b0514e59c86ec.js"
 );
 
 self.addEventListener('message', (event) => {
